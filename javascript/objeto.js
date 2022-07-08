2b393b3a931bd94f33f93abd40b2a7b91b41dd2d
otakusf <santacruz.freddy@gmail.com>
2022-07-07 23:16:38 -0500

    estudiado: operadores en javaScript

--- javascript/objeto.js
+++ javascript/objeto.js
@@ -18,10 +18,30 @@ const otakusf = {
   },
   saludar: function () {
     console.log(`hola :) ${this.name}`);
-    return "hola mundo"
+    return "hola mundo";
   },
+  decirNombre: () => {
+    return `hola me llamo: ${this.name}`;
+  },
+  decirNickName: function () {
+    return `mi nickName es: ${this.nickName}`;
+  }
 };
 
-console.log(otakusf)
+console.log(otakusf);
 
-console.log(otakusf.saludar())
+console.log(otakusf.saludar());
+
+console.log(otakusf.decirNickName())
+
+
+//metodos utiles de objetos
+
+//metodos sale los nombres de las propiedades sean atributos o metodos
+console.log(Object.keys(otakusf))
+
+//devuelve los valores de cada propiedad
+console.log(Object.values(otakusf))
+
+//evalua si el nombre de propiedad existe en el objeto
+console.log(otakusf.hasOwnProperty("name"))
---
+++ javascript/operadores.js
@@ -0,0 +1,55 @@
+//revision de tipos de operadores en javaScript
+
+//aritmeticos + - * / () %
+
+let a = 5 + (5 - 10) * 3;
+
+let b = 5 % 2;
+
+console.log(a);
+
+console.log(b);
+
+//relacionales
+// >,<, >=, <=, ==, ===, !=, !==
+console.log(8 > 9);
+console.log(9 > 8);
+
+// = -> sirve para asignar un valor,ejemplo a = "hola mundo"
+// == -> comparacion de valores
+// === -> 3 iguales es comparacion de tipo de dato y de valor
+
+//valores truely, falsely
+
+console.log(7 == 7);
+console.log("7" == 7);
+console.log(0 == false);
+
+console.log(7 === 7);
+console.log("7" === 7);
+console.log(0 === false);
+
+//operadores incrementales o decrementales
+
+console.log("operadores incrementales");
+let i = 1;
+i = i + 2;
+console.log(i);
+i += 3;
+console.log(i);
+i -= 2;
+console.log(i);
+i /= 2;
+console.log(i);
+i *= 5;
+console.log(i);
+
+//operador unario
+i++;//asignar despues de usar
+console.log(i);
+i--;//restar despues de usar
+console.log(i);
+++i;//asignar antes de usar
+console.log(i);
+--i;//restar antes de usar
+console.log(i);
